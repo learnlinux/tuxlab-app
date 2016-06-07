363cb2c17fe62657033de8113757827130e8a5b0
DerekTBrown <derek@allderek.com>
2016-06-07 16:51:57 -0400

    Updated Login Page

--- imports/startup/server/auth.js
+++ imports/startup/server/auth.js
@@ -3,6 +3,8 @@
   var tuxconf = {};
   tuxconf = JSON.parse(Assets.getText("tuxlab.json"));
 
+// Validate that Google Auth Users are from Domain
+
 // Configure Google Auth
   ServiceConfiguration.configurations.remove({
     service: "google"
@@ -13,3 +15,18 @@
     clientId: tuxconf.google_client,
     secret: tuxconf.google_secret
   });
+
+// Configure Login Validation
+  Accounts.validateLoginAttempt(function(params){
+    switch (params.type){
+      case "google":
+
+        // Verify Google Domain
+        var google_user = params.user.services.google;
+        return (google_user.email.match(new RegExp(tuxconf.google_domain)));
+
+      default:
+        throw new Meteor.Error(403, 'Non-Google Logins are not Supported.');
+        return false;
+    }
+});
