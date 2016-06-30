be4cfcfaffe6716b565cae6beb474f411c90c34b
Derek Brown <derek@allderek.com>
2016-06-30 17:05:14 -0400

    Updated to use new Wetty code (#57)

--- client/imports/ui/components/markdown/markdown.ts
+++ client/imports/ui/components/markdown/markdown.ts
@@ -3,32 +3,32 @@
 	import { Mongo } from 'meteor/mongo';
 	import 'reflect-metadata';
 	import 'zone.js/dist/zone';
-	
-// Angular Imports 
+
+// Angular Imports
 	import { Component, ViewEncapsulation, provide } from '@angular/core';
 	import { bootstrap } from 'angular2-meteor-auto-bootstrap';
 	import { APP_BASE_HREF } from '@angular/common';
 	import { HTTP_PROVIDERS } from '@angular/http';
 	import { InjectUser } from 'angular2-meteor-accounts-ui';
-		
+
 // Angular Material Imports
 	import { MATERIAL_PROVIDERS, MATERIAL_DIRECTIVES } from 'ng2-material';
 	import { MeteorComponent } from 'angular2-meteor';
 	import { OVERLAY_PROVIDERS } from '@angular2-material/core/overlay/overlay';
-		
+
 // Toolbar
   	import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
 	import "../../../../../node_modules/@angular2-material/toolbar/toolbar.css";
 
 // Icon
   	import { MD_ICON_DIRECTIVES, MdIconRegistry } from '@angular2-material/icon';
-	  
+
 // Markdown Imports
 	import * as marked from 'marked';
-	
+
 // Define Tasks Collection
 	let Tasks = new Mongo.Collection('Tasks');
-	
+
 // Define Markdown Component
 	@Component({
 		selector: 'tuxlab-markdown',
@@ -41,18 +41,18 @@
 		encapsulation: ViewEncapsulation.None
 	})
 
-// Export MarkdownView Class 
+// Export MarkdownView Class
 	export class MarkdownView {
 		// Replace with markdown from the database
 		data = "# Markdown\n This is a short **markdown** string, and this is *italic* text. Here is some ***bold and italic*** text.  \n ## Subtitle here";
 		convertedData = this.data;
-		
+
 		constructor(mdIconRegistry: MdIconRegistry) {
 			// Create Icon Font
 			mdIconRegistry.registerFontClassAlias('tux', 'tuxicon');
 			mdIconRegistry.setDefaultFontSetClass('tuxicon');
-			
-			// Parse markdown string 
+
+			// Parse markdown string
 			let md = marked.setOptions({});
 			this.convertedData = md.parse(this.data);
 		}
