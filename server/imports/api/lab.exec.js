a34d0e634e253e01a0637aacd169f7354d5f32f3
Cem Ersoz <cersozz@gmail.com>
2016-07-07 11:53:27 -0400

    issue #84 (#90)
    * issue 32 fixed
    
    * removed swp
    
    * fixed nconf issues
    
    * issue 82, 84 fixes
    
    * issue 32 fixed
    
    * removed swp
    
    * fixed nconf issues
    
    * issue 82, 84 fixes
    
    * fixed #82

--- server/imports/api/lab.exec.js
+++ server/imports/api/lab.exec.js
@@ -1,12 +1,10 @@
 var _eval = require('eval');
-var mongo = require('mongodb');
-var mongoClient = mongo.MongoClient;
-var url = "mongodb://tuxlab:cQPD9wte@ds025792.mlab.com:25792/tuxlab";
 
 var labExec = function(){
-  this.env = require('./env.js');
+  this.env = require('./lab.env.js');
 };
 
+//TODO: move to somewhere else
 labExec.prototype.check = function(str){
   if(!str) { return false; }
     try {
@@ -27,48 +25,85 @@ labExec.prototype.check = function(str){
            (tux.newTask.toString() === tuxOrig.newTask.toString());
 }
 
+/* init: pulls labFile and initializes labExec object from it
+ */
 labExec.prototype.init = function(user,courseId,labId){
   var slf = this;
   this.env.setUser(user);
-  labMongo.importLab(courseId,labId,function(lab){
-    if(!slf.check(lab.file)){
-      console.log("labFile corrupt");
-    }
-    else{
-      slf.tuxlab = eval(lab.file);
-      console.log("success!");
-      console.log(slf.tuxlab);
-    }
-  });
+  var course = Collections.courses.find({_id: courseId});
+  var lab_id = course.labs[labId];
+  var lab = Collections.courses.find({_id: lab_id});
+  slf.tuxlab = _eval(lab.labfile);
+  slf.tuxlab.taskNo = 0;
+  callback(null,slf.parseTasks());
 }
 
 labExec.prototype.parseTasks = function(){
-  return this.tuxlab.taskList.map(function(task){ return {title: task.title,
-							  markdown: task.markdown,
-							  }; });
+  var slf = this;
+  return this.tuxlab.taskList.map(function(task,i){ 
+    if(i <= slf.tuxlab.taskNo){
+      return {title: task.title, markdown: task.markdown};
+    }
+    return {title: task.title, markdown: null};
+  });
 }
 
+/* start: runs setup and moves task header to first task
+ * runs callback(err,res) on err if there is an error,
+ * (null,parseTasks) no error
+ */
 labExec.prototype.start = function(callback){
   var slf = this;
+  this.tuxlab.taskNo = 1;
   this.tuxlab.setup(this.env)
     .then(function(){ slf.tuxlab.tasks(this.env); })
     var tasks = this.tuxlab.tasks(this.env);
-  if(!this.tuxlab.currentTask.next) throw "Tasks not properly chained";
+  if(!this.tuxlab.currentTask.next){
+    TuxLog.log('labfile_error','labfile tasks not properly chained at start');
+    callback("Internal error",null);
+  } 
   this.tuxlab.currentTask = this.tuxlab.currentTask.next;
-  this.currentTask.sFn().then(function(){ callback(slf.parseTasks); });
+  this.currentTask.sFn().then(function(){ callback(null,slf.parseTasks(0)); });
 }
 
+/* next: verifies that task is completed 
+ * moves on to next task and runs callback(null,parseTasks) if completed
+ * runs callback(err,null) -err from verify- if not
+ */
 labExec.prototype.next = function(callback){
   var slf = this;
-  if(this.tuxlab.currentTask.isLast()) throw "cannot call next on last task";
-  currentTask.vFn().then(function(){ slf.tuxlab.currentTask = slf.tuxlab.currentTask.next;
-				     slf.tuxlab.currentTask.sFn().then(function(){
-					callback(slf.tuxlab.currentTask);})},
-			 function(){ /*handle the error*/ }); 
+  if(this.tuxlab.currentTask.isLast()){ 
+    TuxLog.log("debug","trying to call nextTask on last task");
+    callback("Internall error",null);    
+  }
+  this.tuxlabcurrentTask.vFn().then(function(){ 
+                           slf.tuxlab.currentTask = slf.tuxlab.currentTask.next;
+                           slf.tuxlab.currentTask.sFn()
+                             .then(function(){
+                               slf.tuxLab.taskNo += 1;
+			       callback(null,slf.parseTasks());})
+                         },
+			 function(err){ 
+			   callback(err,null);
+			 }); 
 }
 
-labExec.prototype.end = function(){
-//same as next, will change after testing
+/* end: verifies that last task is completed
+ * runs callback on (null,done) if verify runs correctly
+ * runs callback (err, null) if not
+ */
+labExec.prototype.end = function(callback){
+  var slf = this;
+  if(!this.tuxlab.currentTask.isLast()){
+    TuxLog.log("debug","trying to call end on a non-last task");
+    callback("Internal error",null); //TODO: should we allow end on non-last tasks? @Derek
+  }
+  this.tuxlab.currentTask.vFn().then(function(){
+                                       callback(null,"done");
+                                     },
+                                     function(){
+				       callback(err,null);
+                                     });
 }
 
 module.exports = new labExec();
--- server/imports/lab/methods.ts
+++ server/imports/lab/methods.ts
@@ -1,9 +1,6 @@
 declare var Collections : any;
 Meteor.methods({
-  'test'(){
-    return "hello";
-  },
-  'createLab': function(courseId : String,labId : Number){
+  'prepareLab': function(courseId : String,labId : Number){
     var course = Collections.courses.findOne({_id: courseId}).fetch();
     //var t = require('../api/labExec.js');
     return course.labs.find((l) => { return l._id == labId; });
