df18c7b956705d058c9c5f868a739e4008e7c9fa
DerekTBrown <derek@allderek.com>
2016-07-08 13:50:36 -0400

    added start for reactive publication

--- collections/users.ts
+++ collections/users.ts
@@ -58,60 +58,60 @@ if (Meteor.isServer){
   INTERFACE
 **/
 
-/***
-  USER ROLES
-***/
-export class Roles {
+  /***
+    USER ROLES
+  ***/
+  export class Roles {
 
-  /*
-    Determines if user is logged in
-  */
-  static isLoggedIn(){
-    let user : any = Meteor.user();
+    /*
+      Determines if user is logged in
+    */
+    static isLoggedIn(){
+      let user : any = Meteor.user();
 
-    return (typeof user !== 'undefined' && <any> user.roles !== 'undefined');
-  }
+      return (typeof user !== 'undefined' && <any> user.roles !== 'undefined');
+    }
 
-  /*
-    Determines if the user is a student in a particular course
-  */
-  static isStudentFor(courseid : string, userid? : number){
-    let user : any = (typeof userid !== "undefined") ? Meteor.users.findOne(userid) : Meteor.user();
+    /*
+      Determines if the user is a student in a particular course
+    */
+    static isStudentFor(courseid : string, userid? : number){
+      let user : any = (typeof userid !== "undefined") ? Meteor.users.findOne(userid) : Meteor.user();
 
-    if(this.isLoggedIn() && typeof user.roles.student === "array"){
-      for (var i = 0; i < user.roles.student.length; i++){
-        if (user.roles.student[i][0] === courseid) {
-          return true;
+      if(this.isLoggedIn() && typeof user.roles.student === "array"){
+        for (var i = 0; i < user.roles.student.length; i++){
+          if (user.roles.student[i][0] === courseid) {
+            return true;
+          }
         }
+        return false;
       }
-      return false;
+      else{
+        return false;
+      }
     }
-    else{
-      return false;
+
+    /*
+      Determines if the user is an instructor for a particular course
+    */
+    static isInstructorFor(courseid : string, userid? : number){
+      let user : any = (typeof userid !== "undefined") ? Meteor.users.findOne(userid) : Meteor.user();
+      return (this.isLoggedIn() && typeof user.roles.instructor === "array" && user.roles.instructor.contains(courseid));
     }
-  }
 
-  /*
-    Determines if the user is an instructor for a particular course
-  */
-  static isInstructorFor(courseid : string, userid? : number){
-    let user : any = (typeof userid !== "undefined") ? Meteor.users.findOne(userid) : Meteor.user();
-    return (this.isLoggedIn() && typeof user.roles.instructor === "array" && user.roles.instructor.contains(courseid));
-  }
+    /*
+      Determines if the user is an administrator for a course
+    */
+    static isAdministratorFor(courseid : string, userid? : number){
+      let user : any = (typeof userid !== "undefined") ? Meteor.users.findOne(userid) : Meteor.user();
+      return (this.isLoggedIn() && typeof user.roles.administrator === "array" && (user.roles.administrator.contains('global') || user.roles.administrator.contains(courseid)));
+    }
 
-  /*
-    Determines if the user is an administrator for a course
-  */
-  static isAdministratorFor(courseid : string, userid? : number){
-    let user : any = (typeof userid !== "undefined") ? Meteor.users.findOne(userid) : Meteor.user();
-    return (this.isLoggedIn() && typeof user.roles.administrator === "array" && (user.roles.administrator.contains('global') || user.roles.administrator.contains(courseid)));
-  }
-
-  /*
-    Determines if the user is a global administrator
-  */
-  static isGlobalAdministrator(userid? : number){
-    let user : any = (typeof userid !== "undefined") ? Meteor.users.findOne(userid) : Meteor.user();
-    return (this.isLoggedIn() && typeof user.roles.administrator === "array" && user.roles.administrator.contains('global'));
-  }
-};
+    /*
+      Determines if the user is a global administrator
+    */
+    static isGlobalAdministrator(userid? : number){
+      let user : any = (typeof userid !== "undefined") ? Meteor.users.findOne(userid) : Meteor.user();
+      return (this.isLoggedIn() && typeof user.roles.administrator === "array" && user.roles.administrator.contains('global'));
+    }
+  };
