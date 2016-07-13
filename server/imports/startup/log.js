ca45db5ae76167303e8037d26147b5b4116f9921
Derek Brown <derek@allderek.com>
2016-07-13 13:09:49 -0400

    Updated versions

--- server/imports/startup/log.js
+++ server/imports/startup/log.js
@@ -7,12 +7,14 @@ var nconf = require('nconf');
 require('winston-mongodb').MongoDB;
 
 var log_collection_name = nconf.get('mongo_log_collection');
-
 logs_collection = new Mongo.Collection(log_collection_name);
 
 TuxLog = new (winston.Logger)({
   transports: [
      new (winston.transports.Console)(),
-     new (winston.transports.MongoDB)({db: logs_collection.rawDatabase(), collection: log_collection_name})
+     new (winston.transports.MongoDB)({
+       db: logs_collection.rawDatabase(),
+       collection: log_collection_name
+     })
    ]
 });
