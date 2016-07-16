e999ae395acd0ce0b2becb2c4128391151153364
Derek Brown <derek@allderek.com>
2016-07-16 03:38:10 -0400

    Fixing bug

--- server/imports/startup/etcd.js
+++ server/imports/startup/etcd.js
@@ -3,21 +3,29 @@
 */
 
 // Get Address
-var etcd_address = 'localhost:2379';
+var etcd_address = '127.0.0.1:2379';
 
 // Get Auth
-var etcd_auth = {
-  user: nconf.get('etcd_user'),
-  password: nconf.get('etcd_pass')
+var etcd_auth;
+if (nconf.get('etcd_user') == null){
+  etcd_auth = {
+    user: nconf.get('etcd_user'),
+    pass: nconf.get('etcd_pass')
+  }
+}
+
+// Options
+var etcd_options = {
+    auth: etcd_auth
 }
 
 // Start ETCD
 var Etcd = require('node-etcd');
-etcd = new Etcd(etcd_address,etcd_auth)
+etcd = new Etcd(etcd_address,etcd_options)
 
 // Create TuxLab Dir
-    etcd.mkdir("tuxlab", function(err){
-        if(err){
-          TuxLog.log("warn", err);
-        }
-    });
+etcd.mkdir("tuxlab", function(err){
+  if(err){
+   TuxLog.log("warn", err);
+  }
+});
