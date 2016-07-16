99bf9674700d0c05263dd5ccac8b8546c62454ba
Derek Brown <derek@allderek.com>
2016-07-16 02:14:48 -0400

    Fix async error

--- server/imports/course/methods.ts
+++ server/imports/course/methods.ts
@@ -1,47 +1,6 @@
-var async = require ('async');
-var future = require('fibers/future');
+import { Meteor } from 'meteor/meteor';
+import { course_search } from './search.ts'
 
 Meteor.methods({
-  'search_courses': function(text : string, results_per_page : number, page_no : number) {
-
-    var courses = Collections.courses;
-
-    var search_pattern = new RegExp(text,"i");
-    if (results_per_page <= 0 || results_per_page > 200) results_per_page = 200;
-
-    var search_object =
-      {$and : [
-        {"hidden" : false},
-        {$or : [
-          {"course_number" : search_pattern},
-          {$where: "this.course_number.replace(/[ .-]/g,'') == '"+text+"'"},
-          {"course_name" : search_pattern}
-        ]}
-      ]};
-
-     var search_options =
-       {limit : results_per_page,
-        skip : [results_per_page, page_no],
-        fields : {
-          "course_number" : 1,
-          "course_name" : 1,
-          "instructor_ids" : 1,
-          "course_description" : 1}
-       };
-
-     var result = new future();
-
-     async.parallel(
-       {
-         "course_count" : function(callback) {callback(null, courses.find(search_object).count());},
-         "course_results" : function(callback) {callback(null, courses.find(search_object, search_options).fetch());}
-       },
-       function(err, results) {
-         if(err)
-            result.throw(err);
-         result.return(results);
-     });
-
-     return result.wait();
-   }
+  'search_courses': Meteor.wrapAsync(course_search);
 });
