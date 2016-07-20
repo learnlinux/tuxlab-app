6a8c5acea2f013832ddf977470c3cd7038b357fc
Sander Shi <sander.hacker@hotmail.com>
2016-07-20 18:15:49 -0400

    delete instructor page
    user schema

--- client/imports/ui/components/explore/search.ts
+++ client/imports/ui/components/explore/search.ts
@@ -1,43 +1,44 @@
 // Meteor Imports
-	import { Meteor } from 'meteor/meteor';
-	import { Mongo }       from 'meteor/mongo';
-	import 'reflect-metadata';
-	import 'zone.js/dist/zone';
+  import { Meteor } from 'meteor/meteor';
+  import { Mongo }       from 'meteor/mongo';
+  import 'reflect-metadata';
+  import 'zone.js/dist/zone';
 
 // Angular Imports
-	import { Component, ViewEncapsulation, provide, Input } from '@angular/core';
-	import { bootstrap } from 'angular2-meteor-auto-bootstrap';
-	import { APP_BASE_HREF, FORM_DIRECTIVES } from '@angular/common';
-	import { HTTP_PROVIDERS } from '@angular/http';
-	import { InjectUser } from 'angular2-meteor-accounts-ui';
+  import { Component, ViewEncapsulation, provide, Input } from '@angular/core';
+  import { bootstrap } from 'angular2-meteor-auto-bootstrap';
+  import { APP_BASE_HREF, FORM_DIRECTIVES } from '@angular/common';
+  import { HTTP_PROVIDERS } from '@angular/http';
+  import { InjectUser } from 'angular2-meteor-accounts-ui';
   import { ROUTER_DIRECTIVES } from '@angular/router';
 
 // Angular Material Imports
-	import { MATERIAL_PROVIDERS, MATERIAL_DIRECTIVES } from 'ng2-material';
-	import { MeteorComponent } from 'angular2-meteor';
-	import { MD_TABS_DIRECTIVES } from '@angular2-material/tabs'
-	import { MD_INPUT_DIRECTIVES } from '@angular2-material/input';
-	import { MdToolbar } from '@angular2-material/toolbar';
+  import { MATERIAL_PROVIDERS, MATERIAL_DIRECTIVES } from 'ng2-material';
+  import { MeteorComponent } from 'angular2-meteor';
+  import { MD_TABS_DIRECTIVES } from '@angular2-material/tabs'
+  import { MD_INPUT_DIRECTIVES } from '@angular2-material/input';
+  import { MdToolbar } from '@angular2-material/toolbar';
 
 // Icon
-	import { MD_ICON_DIRECTIVES, MdIconRegistry } from '@angular2-material/icon';
+  import { MD_ICON_DIRECTIVES, MdIconRegistry } from '@angular2-material/icon';
 
 // Courses Database Imports
-  import { courses } from '../../../../../collections/courses.ts';
+   import { courses } from '../../../../../collections/courses.ts';
 
 // Define SearchView Component
-	@Component({
-		selector: 'tuxlab-searchview',
-		templateUrl: '/client/imports/ui/components/explore/search.html',
-		directives: [ MATERIAL_DIRECTIVES, 
-					  MD_ICON_DIRECTIVES, 
-					  MD_TABS_DIRECTIVES,
-            ROUTER_DIRECTIVES,
-					  MD_INPUT_DIRECTIVES,
-					  MdToolbar ],
-		viewProviders: [ MdIconRegistry ],
-		encapsulation: ViewEncapsulation.None
-	})
+  @Component({
+    selector: 'tuxlab-searchview',
+    templateUrl: '/client/imports/ui/components/explore/search.html',
+    directives: [ MATERIAL_DIRECTIVES, 
+      MD_ICON_DIRECTIVES, 
+      MD_TABS_DIRECTIVES,
+      ROUTER_DIRECTIVES,
+      MD_INPUT_DIRECTIVES,
+      MdToolbar 
+    ],
+    viewProviders: [ MdIconRegistry ],
+    encapsulation: ViewEncapsulation.None
+  })
 	
 // Export Explore Class 
 export class SearchView extends MeteorComponent {
