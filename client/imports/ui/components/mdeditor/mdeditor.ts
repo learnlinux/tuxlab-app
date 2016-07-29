eb1ab70a759c18b12abe282a8e0f6676418c937f
Sander Shi <sander.hacker@hotmail.com>
2016-07-29 11:56:18 -0400

    fixed reversed md text editor

--- client/imports/ui/components/mdeditor/mdeditor.ts
+++ client/imports/ui/components/mdeditor/mdeditor.ts
@@ -22,26 +22,29 @@
 		@Input() mdData: string = "";
 		@Output() mdUpdated = new EventEmitter<string>();
 		public mde;
+    readMDE: boolean = false;
 		constructor(private elementRef:ElementRef) {
 			super();
 		}
 
-		ngAfterViewInit(){
-			var self = this;
-			// Instantiate SimpleMDE
-			this.mde = new SimpleMDE({ element: this.elementRef.nativeElement.value });
-			// Read initial data from task markdown
-			this.mde.value(self.mdData);
-			// Catch changes
-			this.mde.codemirror.on("change", function() {
-				self.mdData = self.mde.value();
-				self.mdUpdated.emit(self.mdData);
-			});
-		}
-		ngOnChanges(changes) {
-			if(typeof this.mde !== "undefined") {
-				this.mde.value(changes['mdData'].currentValue);
-			}
-		}
+    ngAfterViewInit(){
+      var self = this;
+      // Instantiate SimpleMDE
+      this.mde = new SimpleMDE({ element: this.elementRef.nativeElement.value });
+      // Read initial data from task markdown
+      this.mde.value(self.mdData);
+      // Catch changes
+      this.mde.codemirror.on("change", function() {
+        self.mdData = self.mde.value();
+        self.mdUpdated.emit(self.mdData);
+      });
+    }
+    ngOnChanges(changes) {
+      // Only update editor on first defined value
+      if(typeof this.mde !== "undefined" && !this.readMDE) {
+        this.mde.value(changes['mdData'].currentValue);
+        this.readMDE = true;
+      }
+    }
 
 	}
