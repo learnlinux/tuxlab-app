09518dcb1115301c0a685e1373c2987e0f2f10b5
Sander Shi <sander.hacker@hotmail.com>
2016-08-01 11:23:38 -0400

    indentation

--- client/imports/ui/components/mdeditor/mdeditor.ts
+++ client/imports/ui/components/mdeditor/mdeditor.ts
@@ -1,30 +1,30 @@
 // Meteor Imports
-	import { Meteor } from 'meteor/meteor';
-	import { MeteorComponent } from 'angular2-meteor';
+  import { Meteor } from 'meteor/meteor';
+  import { MeteorComponent } from 'angular2-meteor';
 
 // Angular Imports
-	import { Component, ElementRef, ViewChild, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+  import { Component, ElementRef, ViewChild, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 
 // Declare Global Variable
-	var SimpleMDE : any = require('simplemde');
+  var SimpleMDE : any = require('simplemde');
 
 // Define Editor Component
-	@Component({
-		selector: 'tuxlab-mdeditor',
-		templateUrl: '/client/imports/ui/components/mdeditor/mdeditor.html',
-	})
+  @Component({
+    selector: 'tuxlab-mdeditor',
+    templateUrl: '/client/imports/ui/components/mdeditor/mdeditor.html',
+  })
 
 // Export Editor Class
-	export class MDEditor extends MeteorComponent implements OnChanges {
-		@ViewChild('simplemde') textarea : ElementRef;
-		@Input() mdData: string = "";
-		@Output() mdUpdated = new EventEmitter<string>();
-		public mde;
+  export class MDEditor extends MeteorComponent implements OnChanges {
+    @ViewChild('simplemde') textarea : ElementRef;
+    @Input() mdData: string = "";
+    @Output() mdUpdated = new EventEmitter<string>();
+    public mde;
     readMDE: boolean = false;
 
-		constructor(private elementRef:ElementRef) {
-			super();
-		}
+    constructor(private elementRef:ElementRef) {
+      super();
+    }
 
     ngAfterViewInit(){
       var self = this;
