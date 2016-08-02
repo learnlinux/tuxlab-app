6b3dcf40df92c878878eac3dc41f4f23ea3e5bd2
Derek Brown <derek@allderek.com>
2016-08-02 16:59:45 -0400

    debugging wercker

--- client/imports/ui/routes/course.guard.course.ts
+++ client/imports/ui/routes/course.guard.course.ts
@@ -1,4 +1,47 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+import { InjectUser } from 'angular2-meteor-accounts-ui';
+import { Roles } from '../../../../collections/users.ts';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromPromise';
+
+declare var Collections : any;
 
 /**
-  Guards a route by enforcing that the course exists.
+Guards a route by enforcing that the course exists.
 **/
+
+@InjectUser("user")
+@Injectable()
+export class CourseGuardRecord implements CanActivate{
+  user: Meteor.User;
+
+  constructor(private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+    var slf = this;
+
+    // Get Params
+    const course_id : string = (<any>state).parent(route).params.courseid;
+
+    var obs : Observable<boolean> = Observable.fromPromise(new Promise<boolean> (function(resolve, reject){
+
+      Meteor.subscribe('courses', function(){
+
+        // Enroll Authenticated Users
+          var course_record = Collections.course_records.findOne({user_id: slf.user._id, course_id: course_id});
+          if (typeof course_record !== "undefined" && course_record !== null){
+            resolve(true);
+          }
+          else{
+
+          }
+
+      });
+    }));
+
+    return obs;
+  }
+}
