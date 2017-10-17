346f0ade4bb202fa35e7dcabea6ce147dfc44e52
Derek Brown <derek@allderek.com>
2017-10-16 21:10:17 -0400

    Handle no organizational login case

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import * as _ from "lodash";
 import { Meteor } from 'meteor/meteor';
 
 import SetupAuthentication from "./imports/service/auth";
@@ -7,3 +8,10 @@ import { DefaultFixtures } from "../imports/test/fixtures";
 Meteor.startup(() => {
   SetupAuthentication();
 })
+
+// Enable Fixtures if Defined
+if(_.has(process.env, "TUXLAB_FIXTURES") &&
+   typeof process.env["TUXLAB_FIXTURES"] === "string" &&
+   process.env["TUXLAB_FIXTURES"].toUpperCase() === "TRUE"){
+   new DefaultFixtures();
+}
